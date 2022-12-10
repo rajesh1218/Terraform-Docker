5212c5dc50f19a7c9d9aedcfedfbd7c0060253de
rajesh1218 <117444063+rajesh1218@users.noreply.github.com>
2022-12-10 09:52:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.23.1"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+  host = "unix:///var/run/docker.sock"
+}
+resource "docker_image" "ubuntu" {
+  name = var.ubuntu_image
+}
+resource "docker_container" "ubuntu" {
+  name = var.container_name
+  must_run = true
+  command = [
+    "tail",
+    "-f",
+    "/dev/null"
+  ]
+}
